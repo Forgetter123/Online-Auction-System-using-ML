dd33ca70a84fe2cc958f5a8e4da28d6f948d8e00
Forgetter123 <23mp2104@rit.ac.in>
2025-01-25 20:46:46 +0530

    Delete ml directory

--- ml/fraud_detection.py
+++
@@ -1,8 +0,0 @@
-
-from sklearn.ensemble import IsolationForest
-
-def detect_fraud(bidding_data):
-    model = IsolationForest()
-    model.fit(bidding_data)
-    predictions = model.predict(bidding_data)
-    return bidding_data[predictions == -1]
--- ml/user_analysis.py
+++
@@ -1,4 +0,0 @@
-
-def analyze_user_behavior(user_data):
-    user_data['hour'] = user_data['timestamp'].dt.hour
-    return user_data['hour'].value_counts()
