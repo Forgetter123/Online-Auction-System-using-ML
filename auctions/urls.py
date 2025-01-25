2ace8ff468f79d76cf06140b711e58aacd81225d
Forgetter123 <23mp2104@rit.ac.in>
2025-01-25 20:47:01 +0530

    Delete auctions directory

--- auctions/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.auction_list, name='auction_list'),
-    path('auction/<int:auction_id>/', views.auction_detail, name='auction_detail'),
-    path('recommendations/', views.recommendations, name='recommendations'),
-]
